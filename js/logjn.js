e3ea18d4c3ec153a94898d9b03576f380d0657b8
Dhuzi <1638673951@qq.com>
2020-05-08 23:46:32 +0800

    新增js目录

---
+++ js/logjn.js
@@ -0,0 +1,14 @@
+jQuery. get(/path/to/file', (paraml: valuel, function(data, textStatus, xhr) 1
+optional stuff to do after success
+}; s. ajax(
+ur1: '/path/to/file, type: ' default GET (other values: POST).
+dataType: ' default: Intelligent Guess (other values: xml, json, script, or html)',.
+data: (paraml: ' value1"},
+)
+. done(function() {
+))
+console. log ("success");
+: fail(function() {
+)) console. log("error");
+. always(function()
+}); console. log("complete");
